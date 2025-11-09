02a117bb662e04691a2a5dd9c904d97882b6691d
TANIGUCHI Kazushige <taniguchi.kazushige@gmail.com>
2025-11-09 15:05:48 +0900

    Supports batch processing.

--- pytorch/yolov3_scratch/train.py
+++ pytorch/yolov3_scratch/train.py
@@ -9,21 +9,31 @@ from yolov3_dataset import YoloV3_DatasetFromCOCO
 
 criterion_bce = torch.nn.BCEWithLogitsLoss()
 
-def bbox_metric(y_pred, y_true, class_n = 80):
-  for i in range(3):
-    y_pred_cut = y_pred[:,i*(4 + 1 + class_n) :(i+1)*(4 + 1 + class_n) ]
-    y_true_cut = y_true[:,i*(4 + 1 + class_n) :(i+1)*(4 + 1 + class_n) ]
-    loss_coord = torch.sum(torch.square(y_pred_cut[:,0:4] - y_true_cut[:,0:4])*y_true_cut[:,4])
-    loss_obj = torch.sum((-1 * torch.log(torch.sigmoid(y_pred_cut[:,4] )+ 1e-10) + criterion_bce(y_pred_cut[:,5:],y_true_cut[:,5:]))*y_true_cut[:,4]) 
-    loss_noobj =  torch.sum((-1 * torch.log(1 - torch.sigmoid(y_pred_cut[:,4])+ 1e-10))*(1 - y_true_cut[:,4]))
-    return loss_coord , loss_obj , loss_noobj
+def bbox_metric(y_pred, y_true, class_n=80):
+    batch_size = y_pred.shape[0]
+    loss_coord = 0
+    loss_obj = 0
+    loss_noobj = 0
+
+    for i in range(3):
+        y_pred_cut = y_pred[:, i*(4+1+class_n):(i+1)*(4+1+class_n)]
+        y_true_cut = y_true[:, i*(4+1+class_n):(i+1)*(4+1+class_n)]
+
+        # 各サンプルについて損失を加算
+        loss_coord += torch.sum((y_pred_cut[:, 0:4] - y_true_cut[:, 0:4])**2 * y_true_cut[:, 4:5])
+        loss_obj   += torch.sum((-torch.log(torch.sigmoid(y_pred_cut[:, 4:5]) + 1e-10)
+                                 + criterion_bce(y_pred_cut[:, 5:], y_true_cut[:, 5:])) * y_true_cut[:, 4:5])
+        loss_noobj += torch.sum((-torch.log(1 - torch.sigmoid(y_pred_cut[:, 4:5]) + 1e-10))
+                                 * (1 - y_true_cut[:, 4:5]))
+    # バッチ平均を返す
+    return loss_coord / batch_size, loss_obj / batch_size, loss_noobj / batch_size
 
 def main():
   args = sys.argv
   annotation_file = args[1]
   img_size = (416,416)
   train_dataset = YoloV3_DatasetFromCOCO(annotation_file, img_size, bShortSet = False)
-  train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = 1)
+  train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = 4)
 
   transform = T.Compose([T.ToTensor()])
     
@@ -51,19 +61,21 @@ def main():
     
       with tqdm(train_loader) as pbar:
         pbar.set_description("[train] Epoch %d" % epoch)
-        for n, (img_file, scale3_label , scale2_label ,scale1_label) in enumerate(pbar):
+        for n, (img_files, scale3_label , scale2_label ,scale1_label) in enumerate(pbar):
           optimizer.zero_grad()
-          img = cv2.imread(img_file[0])
-          img = cv2.resize(img , img_size)
-          img = Image.fromarray(img)
-          img = transform(img)
-          img = img.cuda()
-          img = img.unsqueeze(0)
+          imgs = []
+          for img_file in img_files:
+            img = cv2.imread(img_file)
+            img = cv2.resize(img , img_size)
+            img = Image.fromarray(img)
+            img = transform(img)
+            imgs.append(img)
+          imgs = torch.stack(imgs).cuda()
           scale1_label = scale1_label.cuda()
           scale2_label = scale2_label.cuda()
           scale3_label = scale3_label.cuda()
           labels = [scale3_label , scale2_label ,scale1_label]
-          preds  = list(model(img))
+          preds  = list(model(imgs))
           loss_coord = 0
           loss_obj = 0
           loss_noobj = 0
